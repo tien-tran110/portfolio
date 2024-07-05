e282590d1764c13e6e13e083b247d0b0a5c249dd
Tien Tran <ttntien1406@gmail.com>
2024-07-05 13:58:49 -0400

    Add automate deployment script

---
+++ redeploy-site.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Kill all existing tmux sessions
+tmux kill-server
+# Navigate to project directory
+cd portfolio
+
+# Fetch and reset git repository
+git fetch && git reset origin/main --hard
+
+# Activate the virtual environment and install dependencies
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+# Create a script to start the Flask server
+echo '#!/bin/bash
+cd portfolio
+source python3-virtualenv/bin/activate
+flask run --host=0.0.0.0' > start-flask.sh
+chmod +x start-flask.sh
+
+tmux new-session -d -s flask_session './start-flask.sh'
